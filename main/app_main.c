b4e3ea6f51e11b250b1aeaaef8e4b6011f113925
ottarfinns <ottarfinns@gmail.com>
2025-09-04 20:11:25 +0000

    Ottar initial commit

--- main/app_main.c
+++ main/app_main.c
@@ -17,40 +17,39 @@ const char* ERROR_UNKNOWN = "ERROR // PROCESSING FAILURE";
 const char* ERROR_COMMAND = "Command error";
 const char* ERROR_ARGUMENT = "Argument error";
 
-void app_main(void)
-{
-	char msg_in[MSG_BUFFER_LENGTH];
-	char msg_out[MSG_BUFFER_LENGTH];
+void app_main(void) {
+  char msg_in[MSG_BUFFER_LENGTH];
+  char msg_out[MSG_BUFFER_LENGTH];
 
-	serial_write_line(MESSAGE_SYNC);
-	while (true) {
-		memset(msg_in, 0, MSG_BUFFER_LENGTH);
-		memset(msg_out, 0, MSG_BUFFER_LENGTH);
+  serial_write_line(MESSAGE_SYNC);
+  while (true) {
+    memset(msg_in, 0, MSG_BUFFER_LENGTH);
+    memset(msg_out, 0, MSG_BUFFER_LENGTH);
 
-		serial_write_prompt();
-		int count = serial_read_line(msg_in);
-		if (count < 0) {
-			serial_write_line(ERROR_OVERRUN);
-		} else if (count == 0) {
-			// Do nothing.  Allow loop to continue and write a new prompt.
-		} else { // count > 0
-			int processing_result = process_command(msg_in, msg_out);
-			switch (processing_result) {
-				case 0:	// Successful processing.
-					serial_write_line(msg_out);
-					break;
-				
-				case 1: // Command error.
-					serial_write_line(ERROR_COMMAND);
-					break;
+    serial_write_prompt();
+    int count = serial_read_line(msg_in);
+    if (count < 0) {
+      serial_write_line(ERROR_OVERRUN);
+    } else if (count == 0) {
+      // Do nothing.  Allow loop to continue and write a new prompt.
+    } else {  // count > 0
+      int processing_result = process_command(msg_in, msg_out);
+      switch (processing_result) {
+        case 0:  // Successful processing.
+          serial_write_line(msg_out);
+          break;
 
-				case 2: // Argument error.
-					serial_write_line(ERROR_ARGUMENT);
-					break;
+        case 1:  // Command error.
+          serial_write_line(ERROR_COMMAND);
+          break;
 
-				default: // Unrecognized result.
-					serial_write_line(ERROR_UNKNOWN);
-			}
-		}
-	}
+        case 2:  // Argument error.
+          serial_write_line(ERROR_ARGUMENT);
+          break;
+
+        default:  // Unrecognized result.
+          serial_write_line(ERROR_UNKNOWN);
+      }
+    }
+  }
 }
